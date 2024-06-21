7bcc7c1b06d78bf01b1f2db029923fb28e0fde5c
Hingaz <hhinga04@gmail.com>
2024-06-21 14:39:07 +0300

    Footer

--- restaurant/src/components/Footer/Newsletter.jsx
+++ restaurant/src/components/Footer/Newsletter.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import './Newsletter.css';
+import "./Newsletter.css";
+import SubHeading from "../SubHeading/SubHeading";
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className="app__newsletter">
+    <div className="app__newsletter-heading">
+      <SubHeading title="Newsletter" />
+    </div>
+    <h1 className="foot">Hi there</h1>
   </div>
 );
-
 export default Newsletter;
